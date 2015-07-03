984922ac8a6338f84160e7e3df53eb49461b5ab0
Edward-L <linzhexi777@gmail.com>
2015-07-03 15:53:56 +0800

    getseo

---
+++ PageRank.py
@@ -0,0 +1,41 @@
+import requests
+import re
+
+GPR_HASH_SEED = "Mining PageRank is AGAINST GOOGLE'S TERMS OF SERVICE. YES, I'm talking to you, scammer."
+
+def google_hash(value):
+	magic = 0x1020345
+	for i in xrange(len(value)):
+		magic ^= ord(GPR_HASH_SEED[i % len(GPR_HASH_SEED)]) ^ ord (value[i])
+		magic = (magic >> 23 | magic << 9) & 0xFFFFFFFF
+		return "8%08x" % (magic)
+
+def getPR(www):
+	try:
+		url = 'http://toolbarqueries.google.com/tbr?client=navclient-auto&ch=%s&features=Rank&q=info:%s' % (google_hash(www) , www)
+		response = requests.get(url)
+		rex = re.search(r'(.*?:.*?:)(\d+)', response.text)
+		return rex.group(2)
+	except:
+		return None
+
+def getBR(www):
+	try:
+		url = 'http://mytool.chinaz.com/baidusort.aspx?host=%s&sortType=0' % ( www , )
+		response = requests.get(url)
+		data = response.text
+		rex = re.search(r'(<div class="siteinfo">).+?<font.+?>(\d*?)(</font>)', data,re.I)
+		return rex.group(2)
+	except:
+		return None
+
+def main():
+	www = raw_input("pleace input the Domain name:")
+	# print www
+	# print google_hash(www)
+	# print getPR(www)
+	print getBR(www)
+	return
+
+if __name__ == '__main__':
+	main()
